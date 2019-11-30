d6eec9a6e36f33d20667bb295603f275b7041332
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2019-11-30 15:42:01 -0800

    azure pipeline

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,8 @@
 library(testthat)
 library(prismatic)
 
-test_check("prismatic")
+if (requireNamespace("xml2")) {
+  test_check("prismatic", reporter = MultiReporter$new(reporters = list(JunitReporter$new(file = "test-results.xml"), CheckReporter$new())))
+} else {
+  test_check("prismatic")
+}
