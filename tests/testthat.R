9ec1b8358348c7aa2d2cd5326cbf72a3a9ead699
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2019-11-30 16:09:29 -0800

    remove azure stuff

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,4 @@
 library(testthat)
 library(prismatic)
 
-if (requireNamespace("xml2")) {
-  test_check("prismatic", reporter = MultiReporter$new(reporters = list(JunitReporter$new(file = "test-results.xml"), CheckReporter$new())))
-} else {
-  test_check("prismatic")
-}
+test_check("prismatic")
